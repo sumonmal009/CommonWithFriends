4fca7d44917261697d3455dc5cebacd96d3dbd6e
Sumon.Mal <A582575@corp.target.com>
2016-06-24 17:08:10 +0530

    code changes for FixedThreadPool

---
+++ Concurrency/src/concurrency/fixedThreadPool/Impl/FixedThreadPool.java
@@ -0,0 +1,22 @@
+package concurrency.fixedThreadPool.Impl;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import concurrency.fixedThreadPool.Task.WorkerThread;
+
+public class FixedThreadPool {
+
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 10; i++) {
+            Runnable worker = new WorkerThread("" + i);
+            executor.execute(worker);
+          }
+        executor.shutdown();
+        while (!executor.isTerminated()) {
+        }
+        System.out.println("Finished all threads");
+    }
+
+}
